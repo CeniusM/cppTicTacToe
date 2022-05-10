c0055d9b8e666794b2f91cc2d6dbbeb591c0d1ae
CeniusM <cenius.meier@gmail.com>
2022-05-10 19:37:19 +0200

    changes

--- c++TicTacToe/Board.cpp
+++ c++TicTacToe/Board.cpp
@@ -3,7 +3,69 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+bool Board::MakeMove(int move)
+{
+	if (board[move] != 0 || isGameOver)
+		return false;
+
+	board[move] = player;
+	turn++;
+
+	if (turn == 9)
+	{
+		winner = 0;
+		isGameOver = true;
+		return true;
+	}
+	if (GameOverCheck())
+	{
+		winner = player;
+		isGameOver = true;
+		return true;
+	}
+
+	player ^= 0b11; // change player
+}
+
+bool Board::GameOverCheck()
+{
+	int j;
+	for (int i = 0; i < 3; i++)
+	{
+		j = i * 3;
+		if (board[i] != 0 && board[i] == board[i + 3] && board[i + 3] == board[i + 6])
+			return true;
+		if (board[j] != 0 && board[j] == board[j + 1] && board[j + 1] == board[j + 2])
+			return true;
+	}
+
+	if (board[0] != 0 && board[0] == board[4] && board[4] == board[8])
+		return true;
+
+	if (board[2] != 0 && board[2] == board[4] && board[4] == board[6])
+		return true;
+
+	return false;
+}
+
+Board::Board()
+{
+
+}
+
+Board::~Board()
+{
+
+}
+
+
+
+
+/*
+#include "Board.h"
+
+#include <iostream>
+#include <string>
 
 bool Board::MakeMove(int move)
 {
@@ -59,3 +121,4 @@ Board::~Board()
 {
 	//free(board);
 }
+*/
--- c++TicTacToe/Board.h
+++ c++TicTacToe/Board.h
@@ -6,6 +6,35 @@ class Board
 {
 public:
     int board[9]{};
+    int player = 1;
+    int turn = 0;
+    int winner = 0;
+    bool isGameOver = false;
+
+    bool MakeMove(int move);
+
+    bool GameOverCheck();
+
+    Board();
+    ~Board();
+
+private:
+
+};
+
+
+
+
+
+/*
+#pragma once
+
+
+
+class Board
+{
+public:
+    int board[9]{};
     int player;
     int turn;
     int winner;
@@ -21,3 +50,4 @@ public:
 private:
 
 };
+*/
