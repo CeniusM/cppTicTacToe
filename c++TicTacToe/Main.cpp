8200f1b4f207d9570d413d947152df31be3b5d10
CeniusM <cenius.meier@gmail.com>
2022-06-02 17:42:47 +0200

    changes

--- c++TicTacToe/Main.cpp
+++ c++TicTacToe/Main.cpp
@@ -21,69 +21,79 @@ int main()
 	//    std::cout << "key: " << key.uChar.AsciiChar
 	//        << " code:  " << key.wVirtualKeyCode << std::endl;
 	//}
-	KEY_EVENT_RECORD key;
-
-	Board* board = new Board;
-	bool moveSucces = true;
-	int selecktedPiece = 0;
-
-	// !!! for debbuging
-	//board->MakeMove(selecktedPiece);
-
-	PrintBoard(board->board, &selecktedPiece);
-
-	while (!board->isGameOver)
+	while (true)
 	{
-		if (!moveSucces)
-			cout << "It must be a num from and too 0 - 8, and the spot can not be taken" << endl;
 
-		getconchar(key);
-		//        system("CLS");
+		KEY_EVENT_RECORD key;
 
-		switch (key.uChar.AsciiChar)
-		{
-		case 'w':
-			if (selecktedPiece % 3 != 0)
-				selecktedPiece -= 1;
-			break;
-		case 'a':
-			if (!(selecktedPiece - 3 < 0))
-				selecktedPiece -= 3;
-			break;
-		case 's':
-			if (selecktedPiece != 8)
-				selecktedPiece++;
-			break;
-		case 'd':
-			if (!(selecktedPiece + 3 > 8))
-				selecktedPiece += 3;
-			break;
-		default:
-			break;
-		}
+		Board* board = new Board;
+		bool moveSucces = true;
+		int selecktedPiece = 0;
+
+		// !!! for debbuging
+		//board->MakeMove(selecktedPiece);
 
 		PrintBoard(board->board, &selecktedPiece);
 
-		if (key.wVirtualKeyCode == 32)
-			moveSucces = board->MakeMove(selecktedPiece);
+		while (!board->isGameOver)
+		{
+			if (!moveSucces)
+				cout << "It must be a num from and too 0 - 8, and the spot can not be taken" << endl;
+
+			getconchar(key);
+			//        system("CLS");
+
+			switch (key.uChar.AsciiChar)
+			{
+			case 'w':
+				if (selecktedPiece % 3 != 0)
+					selecktedPiece -= 1;
+				break;
+			case 'a':
+				if (!(selecktedPiece - 3 < 0))
+					selecktedPiece -= 3;
+				break;
+			case 's':
+				if (selecktedPiece != 8)
+					selecktedPiece++;
+				break;
+			case 'd':
+				if (!(selecktedPiece + 3 > 8))
+					selecktedPiece += 3;
+				break;
+			default:
+				break;
+			}
+
+			PrintBoard(board->board, &selecktedPiece);
+
+			if (key.wVirtualKeyCode == 32)
+				moveSucces = board->MakeMove(selecktedPiece);
+		}
+
+
+		switch (board->winner)
+		{
+		case 0:
+			cout << "what a game, well played both of you" << endl;
+			break;
+		case 1:
+			cout << "Well played the the player with [X] :D" << endl;
+			break;
+		case 2:
+			cout << "Well played the the player with [O] :D" << endl;
+			break;
+		default:
+			cout << "what the heck" << endl;
+			break;
+		}
+
+		delete board;
+
+
+		cout << "press 'r' to play again" << endl;
+		getconchar(key);
+		if (key.uChar.AsciiChar != 'r')
+			break;
 	}
-
-
-	switch (board->winner)
-	{
-	case 0:
-		cout << "what a game, well played both of you" << endl;
-		break;
-	case 1:
-		cout << "Well played the the player with [X] :D" << endl;
-		break;
-	case 2:
-		cout << "Well played the the player with [O] :D" << endl;
-		break;
-	default:
-		cout << "what the heck" << endl;
-		break;
-	}
-
-	delete board;
 }
